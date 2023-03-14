135ee0d21337c9630103ebfd931f43714e1f5ac9
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-14 18:43:29 +0000

    scraping id targeted element

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scraper():
+def scraper(url):
     """Scrape the source from URL"""
-    response = requests.get(URL, headers=HEADERS)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
 if __name__ == '__main__':
-    print(scraper())
+    scraped = scraper(URL)
+    extracted = extract(scraped)
+    print(extracted)
